8d20996b56e9fba2a4626461b12907d368f0fb71
bilalmukhtiar5 <billuedwardian08@gmail.com>
2023-09-10 10:09:27 +0500

    add tasks

---
+++ script.js
@@ -0,0 +1,58 @@
+//Question 2
+//part 1
+
+
+let userAge = 17;
+if (userAge >= 18) {
+    console.log('the user is old enough to vote');
+} else {
+    console.log("üser can not vote");
+}
+
+// part 2 
+
+
+let userGrade = "C";
+if(userGrade === "A"){
+    console.log("Great Job");
+}else if(userGrade === "B") {
+    console.log("Great");
+}else if(userGrade === "C") {
+    console.log("Excellent");
+}else if(userGrade === "D") {
+    console.log("Work hard");
+}else if(userGrade === "F") {
+    console.log("Sorry You are fail");
+}
+
+
+// part 3 
+/*
+let userAge = 4;
+if (userAge >= 0 && userAge <= 2) {
+  console.log("baby");
+} else if (userAge >= 3 && userAge <= 5) {
+    console.log("Toddler");
+} else if (userAge >= 6 && userAge <= 12) {
+    console.log("Child");
+} else if (userAge >= 13 && userAge <= 18) {
+    console.log("Teenager");
+} else {
+    console.log("Adult");
+};
+*/
+
+
+
+// part 4
+/*
+let userNumber = 0;
+if (userNumber == 0){
+    console.log("The number is zero");
+} else if (userNumber > 0) {
+    console.log("The number is positive");
+} else if (userNumber < 0) {
+    console.log("The number is Negative");
+}
+
+*/
